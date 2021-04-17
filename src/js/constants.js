4b58e609b6b8d90092112dc528a10c9d61edc181
Tan Dang <tandm160797@gmail.com>
2021-04-17 21:27:37 +0700

    Create constants.js file - support constants for app

---
+++ src/js/constants.js
@@ -0,0 +1,7 @@
+const JOKE_ALL_RATED = 'joke-all-rated';
+const JOKE_NEXT_INDEX_RATED = 'joke-next-index-rated';
+const JOKE_RATED_TTL = 24 * 60 * 60 * 1000;
+
+const HOST = 'http://localhost:3001';
+
+export { JOKE_ALL_RATED, JOKE_NEXT_INDEX_RATED, JOKE_RATED_TTL, HOST };
